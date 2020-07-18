b4723ca92cddcf62dcdeaf39753974e41d8f9e98
123456789Mmoh <mmmmali654321@gmail.com>
2020-07-18 10:42:12 +0100

    first commit

---
+++ question.js
@@ -0,0 +1,29 @@
+var questions=[
+{
+"question":" Whate is name of frist computer designed by charles Babgges?",
+"option1":"calculate",
+"option2":"processor",
+"option3":"abacus" ,
+"option4":"analytical engine",
+"answer":"4"
+},
+{
+    "question":" Whate is name of frist computer designed by charles Babgges",
+    "option1":"calculate",
+    "option2":"processor",
+    "option3":"abacus" ,
+    "option4":"analytical engine",
+    "answer":"4"
+
+
+}
+
+
+
+
+
+
+
+
+
+];
---
+++ quizz-script.js
@@ -0,0 +1,51 @@
+var currentQuestion=0;
+var score=0;
+var totalQuestions=questions.length;
+var container=document.getElementById('quizzContainer');
+var questionEl=document.getElementById('question');
+var opt1=document.getElementById('opt1');
+var opt2=document.getElementById('opt2');
+var opt3=document.getElementById('opt3');
+var opt4=document.getElementById('opt4');
+var nextButton=document.getElementById('nextButton');
+var result=document.getElementById('result');
+function loadQuestion(questionIndex) {
+var q=questions[questionIndex];
+questionEl.textContent=(questionIndex+1)+'.'+ q.question;
+opt1.textContent=q.option1; 
+opt2.textContent=q.option2;
+opt3.textContent=q.option3;
+opt4.textContent=q.option4; 
+
+
+    
+}
+function loadNextQuestion() {
+    var selectedOption=document.querySelector('input[type=radio]:checked');
+    if(!selectedOption)
+    {
+        alert('Please Select Your Answer');
+        return;
+    }
+    var answer=selectedOption.value;
+    if(questions[currentQuestion].answer==answer)
+    {
+        score+=10;
+    }
+   selectedOption.checked=false;
+   currentQuestion++;
+   if(currentQuestion==totalQuestions-1){
+       nextButton.textContent='Finish';
+
+   }
+   if(currentQuestion==totalQuestions)
+   {
+       container.style.display='none';
+       result.style.display='';
+       result.textContent='Your Score is:'+score;
+   
+   }
+    
+   loadQuestion(currentQuestion);
+}
+loadQuestion(currentQuestion)
